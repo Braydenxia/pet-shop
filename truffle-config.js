0b3688dc8bce7b04aa7513ffefe34172e92c2ad5
braydenxia <braydenxia@gmail.com>
2022-05-09 21:58:56 -0700

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "glory beauty edge art zone life enlist common put lucky repeat mesh";
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/6b165dae869a415bbc4a6c1dd99c92ad")
+      },
+      network_id: 3
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
+  }
+};
